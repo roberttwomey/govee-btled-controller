448415509d10b663c9a5f10c2d256418e771ba7b
Robert Twomey <robert.twomey@gmail.com>
2024-04-03 13:15:34 -0700

    added second lightbar

--- descent.py
+++ descent.py
@@ -10,10 +10,16 @@ lightbar2 = '46A48234-B7BF-80C1-8A7F-F66A3FA977B5'
 async def main():
     # Replace this with your LED's MAC address
     thismac = lightbar1
+    # thismac = lightbar1
     led = BluetoothLED(thismac)
+    led2 = BluetoothLED(lightbar2)
+    # led = BluetoothLED(thismac)
     if await led.init_and_connect():
         print("connected {thismac}")
-    
+
+    if await led2.init_and_connect():
+        print("connected {lightbar2}")
+
     # await led.set_state(False) # off
     # time.sleep(1.5)
     # await led.set_state(True) # on
@@ -46,20 +52,28 @@ async def main():
     # OCEAN DESCENT
     # colors https://www.w3.org/TR/css-color-3/#svg-color
     await led.set_brightness(0.5)
+    await led2.set_brightness(0.5)
     await led.set_color_bar('white')
+    await led2.set_color_bar('white')
     time.sleep(3)
     await led.set_color_bar('cyan')
+    await led2.set_color_bar('cyan')
     time.sleep(3)
     await led.set_color_bar('blue')
+    await led2.set_color_bar('blue')
     time.sleep(3)
     await led.set_color_bar('darkblue')
+    await led2.set_color_bar('darkblue')
     time.sleep(3)
     await led.set_color_bar('navy')
+    await led2.set_color_bar('navy')
     time.sleep(3)
     await led.set_color_bar('midnightblue')
+    await led2.set_color_bar('midnightblue')
     # await led.set_brightness(0.1)
     time.sleep(3)
     await led.set_color_bar('black')
+    await led2.set_color_bar('black')
     
 
     # await led.set_color_white(-1.0)
